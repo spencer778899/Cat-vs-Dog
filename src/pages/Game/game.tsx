2d4ee5bec13dfcc729a96ed6decb774997ccbd55
Spencer <spencer778899@gmail.com>
2022-11-01 13:55:12 +0800

    Feat: add game page layout

--- src/pages/Game/game.tsx
+++ src/pages/Game/game.tsx
@@ -1,7 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const GameScreen = styled.div`
+  position: absolute;
+  right: 0;
+  left: 0;
+  display: flex;
+  justify-content: flex-end;
+  flex-wrap: wrap;
+  width: 940px;
+  margin: auto;
+`;
+const GameDogHitPoints = styled.div`
+  width: 50%;
+  margin-top: 20px;
+  text-align: center;
+  font-size: 24px;
+`;
+const GameCatHitPoints = styled.div`
+  width: 50%;
+  margin-top: 20px;
+  text-align: center;
+  font-size: 24px;
+`;
+const GameCanvasSection = styled.div`
+  position: relative;
+  width: 940px;
+  height: 560px;
+`;
+const GameCanvas = styled.canvas`
+  background: #eee;
+  display: block;
+  margin: 0 auto;
+`;
+const GameDog = styled.div`
+  position: absolute;
+  top: 566px;
+  left: 80px;
+  width: 40px;
+  height: 40px;
+  color: #ffffff;
+  background-color: cornflowerblue;
+`;
+const GameCat = styled.div`
+  position: absolute;
+  top: 566px;
+  left: 820px;
+  width: 40px;
+  height: 40px;
+  color: #ffffff;
+  background-color: cornflowerblue;
+`;
 
 function Game() {
-  return <h1>game page</h1>;
+  return (
+    <div>
+      <GameScreen>
+        <GameDogHitPoints>100</GameDogHitPoints>
+        <GameCatHitPoints>100</GameCatHitPoints>
+        <GameCanvasSection>
+          <GameCanvas width={940} height={560} />
+        </GameCanvasSection>
+        <GameDog>dog</GameDog>
+        <GameCat>cat</GameCat>
+      </GameScreen>
+    </div>
+  );
 }
 
 export default Game;
