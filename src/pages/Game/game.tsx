337bffcc7ebeaf008969e58e81e97bc587ec73c1
Spencer <spencer778899@gmail.com>
2022-11-01 15:58:48 +0800

    Feat: Add throwing function logic

--- src/pages/Game/game.tsx
+++ src/pages/Game/game.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import * as React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 
 const GameScreen = styled.div`
@@ -53,15 +54,106 @@ const GameCat = styled.div`
 `;
 
 function Game() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const dogDIV = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const ctx = canvas.current?.getContext('2d');
+    const catX = 840;
+    const catY = 540;
+    let dogX = 100;
+    let dogY = 540;
+    const dogDOM = dogDIV.current;
+    let startTime: number;
+    let timeHandler: NodeJS.Timeout;
+    let startAnimation: NodeJS.Timeout;
+    let time = 1;
+
+    function drawWall() {
+      ctx?.beginPath();
+      ctx?.rect(450, 400, 40, 160);
+      ctx?.fill();
+      ctx?.closePath();
+    }
+
+    function drawDog() {
+      ctx?.beginPath();
+      ctx?.arc(dogX, dogY, 20, 0, Math.PI * 2, false);
+      ctx?.fill();
+      ctx?.closePath();
+    }
+
+    function drawCat() {
+      ctx?.beginPath();
+      ctx?.arc(catX, catY, 20, 0, Math.PI * 2, false);
+      ctx?.fill();
+      ctx?.closePath();
+    }
+
+    function setHostRound() {
+      function getStartTime() {
+        startTime = Number(new Date());
+      }
+
+      function getQuantityOfPower(endTime: number) {
+        const timeLong = endTime - startTime;
+        return timeLong > 2000 ? 10 : 10 * (timeLong / 2000);
+      }
+
+      function stopAnimation() {
+        clearInterval(timeHandler);
+        clearInterval(startAnimation);
+      }
+
+      function startAnimationHandler(quantityOfPower: number) {
+        ctx?.clearRect(0, 0, 940, 560);
+
+        // up data dog coordinate
+        dogX += 10 + quantityOfPower - 0 * time;
+        dogY -= 10 + quantityOfPower - time ** 2;
+
+        // Is dog hit the cat?
+        if (dogX >= 800 && dogX <= 880 && dogY >= 520 && dogY <= 580) {
+          console.log('hit!');
+          stopAnimation();
+        } else if (dogX > 940 || dogX <= 0 || dogY > 560 || dogY < 0) {
+          console.log('miss!');
+          stopAnimation();
+        }
+        drawDog();
+        drawWall();
+      }
+
+      function mouseUpHandler() {
+        const endTime = Number(new Date());
+        const quantityOfPower = getQuantityOfPower(endTime);
+        console.log(quantityOfPower);
+        timeHandler = setInterval(() => {
+          time += 0.06;
+        }, 10);
+        startAnimation = setInterval(() => {
+          startAnimationHandler(quantityOfPower);
+        }, 15);
+        dogDOM?.removeEventListener('mousedown', getStartTime);
+        dogDOM?.removeEventListener('mouseup', mouseUpHandler);
+      }
+      dogDOM?.addEventListener('mousedown', getStartTime);
+      dogDOM?.addEventListener('mouseup', mouseUpHandler);
+    }
+    drawWall();
+    drawDog();
+    drawCat();
+    setHostRound();
+  }, []);
+
   return (
     <div>
       <GameScreen>
         <GameDogHitPoints>100</GameDogHitPoints>
         <GameCatHitPoints>100</GameCatHitPoints>
         <GameCanvasSection>
-          <GameCanvas width={940} height={560} />
+          <GameCanvas width={940} height={560} ref={canvas} />
         </GameCanvasSection>
-        <GameDog>dog</GameDog>
+        <GameDog ref={dogDIV}>dog</GameDog>
         <GameCat>cat</GameCat>
       </GameScreen>
     </div>
