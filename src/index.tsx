f8e9dd1bf0fa47dac0c93dac5e39b9e17d145104
Spencer <spencer778899@gmail.com>
2022-11-04 20:26:05 +0800

    Feat: Add AIgame page router

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Game from './pages/Game/game';
+import AIGame from './pages/AIgame/AIGame';
 import Home from './pages/Home/home';
+import Game from './pages/Game/game';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -13,6 +14,7 @@ root.render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="game" element={<Game />} />
+          <Route path="AIgame" element={<AIGame />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
