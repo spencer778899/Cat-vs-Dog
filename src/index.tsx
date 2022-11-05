07f283418327410fe18f450163c8dea337acd39f
Spencer <spencer778899@gmail.com>
2022-11-05 13:02:34 +0800

    Feat: Add online game page router

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
-import AIGame from './pages/AIgame/AIGame';
+import AIGame from './pages/AIGame/AIGame';
 import Home from './pages/Home/home';
 import Game from './pages/Game/game';
+import OnlineGame from './pages/OnlineGame/onlinegame';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -15,6 +16,7 @@ root.render(
           <Route index element={<Home />} />
           <Route path="game" element={<Game />} />
           <Route path="AIgame" element={<AIGame />} />
+          <Route path="onlinegame" element={<OnlineGame />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
---
+++ src/pages/OnlineGame/onlinegame.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OnlineGame() {
+  return <div>Online Game</div>;
+}
+
+export default OnlineGame;
