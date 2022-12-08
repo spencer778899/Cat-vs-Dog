8a07a4ec0dbf57f7df62287020fa2cb1cd8a5fdf
Spencer <spencer778899@gmail.com>
2022-12-08 18:02:46 +0800

    Refactor: authContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { createGlobalStyle } from 'styled-components';
 import { ToastContainer, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { onSnapshot, doc } from 'firebase/firestore';
-import { AuthContext } from './context/authContext';
-import firestore, { db, realtime } from './utils/firestore';
+import AuthProvider from './context/authContext';
 import Background from './components/background';
 import Navbar from './components/navbar';
 import CheckDevice from './components/checkDevice';
@@ -27,96 +24,10 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [user, setUser] = useState<{
-    uid: string;
-    nickname: string;
-    email: string;
-    photoURL: string;
-    friends: string[] | undefined;
-    changePhotoRight: boolean;
-    inviting: { nickname: string; URL: string; photoURL: string } | undefined;
-  }>({
-    uid: '',
-    nickname: '',
-    email: '',
-    photoURL: '',
-    friends: undefined,
-    changePhotoRight: false,
-    inviting: undefined,
-  });
-
-  useEffect(() => {
-    async function userHandler(auth: { uid: string } | null) {
-      setUser({
-        uid: '',
-        nickname: '',
-        email: '',
-        photoURL: '',
-        friends: undefined,
-        changePhotoRight: false,
-        inviting: undefined,
-      });
-      if (auth) {
-        const userData = await firestore.getUser(auth.uid);
-        setUser({
-          uid: userData?.uid,
-          nickname: userData?.nickname,
-          email: userData?.email,
-          photoURL: userData?.photoURL,
-          friends: userData?.friends,
-          changePhotoRight: userData?.changePhotoRight,
-          inviting: userData?.inviting,
-        });
-        setIsLogin(true);
-        await firestore.updateUserOnline(auth.uid, true);
-        await realtime.setUserIsOnline(auth.uid);
-      } else {
-        setIsLogin(false);
-      }
-    }
-    onAuthStateChanged(getAuth(), userHandler);
-    return () => {
-      onAuthStateChanged(getAuth(), userHandler);
-    };
-  }, [isLogin]);
-
-  useEffect(() => {
-    async function userOnlineStateHandler() {
-      if (!user.uid) return;
-      await firestore.updateUserOnline(user.uid, true);
-      await realtime.setUserIsOnline(user.uid);
-    }
-    document.addEventListener('visibilitychange', userOnlineStateHandler);
-    return () => {
-      document.removeEventListener('visibilitychange', userOnlineStateHandler);
-    };
-  }, [user.uid]);
-
-  useEffect(() => {
-    if (isLogin === false) return;
-    const userSubscriber = onSnapshot(doc(db, 'users', `${user.uid}`), (docs) => {
-      const userData = docs.data();
-      setUser({
-        uid: userData?.uid,
-        nickname: userData?.nickname,
-        email: userData?.email,
-        photoURL: userData?.photoURL,
-        friends: userData?.friends,
-        changePhotoRight: userData?.changePhotoRight,
-        inviting: userData?.inviting,
-      });
-    });
-    return () => {
-      userSubscriber();
-    };
-  }, [isLogin]);
-
-  const userDate = useMemo(() => ({ isLogin, user }), [isLogin, user]);
   return (
     <div>
       <GlobalStyle />
-      <AuthContext.Provider value={userDate}>
+      <AuthProvider>
         <CheckDevice />
         <ToastContainer
           position="top-center"
@@ -135,7 +46,7 @@ function App() {
         <Background />
         <Navbar />
         <Outlet />
-      </AuthContext.Provider>
+      </AuthProvider>
     </div>
   );
 }
